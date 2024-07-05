67643e17908c2e363a2cd69a98b2f454e1dd551f
Emmy <lisariesey@gmail.com>
2024-07-05 21:41:58 +0200

    fix: Ensure EnderPearlListener reacts only to right-click actions

--- src/main/java/dev/revere/hub/enderbutt/EnderPearlListener.java
+++ src/main/java/dev/revere/hub/enderbutt/EnderPearlListener.java
@@ -6,6 +6,7 @@ import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 /**
@@ -18,14 +19,13 @@ public class EnderPearlListener implements Listener {
     @EventHandler
     private void onPearl(PlayerInteractEvent event) {
         Player player = event.getPlayer();
-
-        if (!ConfigHandler.getInstance().getSettingsConfig().getBoolean("enderbutt.enabled")) {
+        if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK) {
             return;
         }
 
-        if (!player.getItemInHand().equals(HotbarItem.ENDER_BUTT.getItem())) {
-            return;
-        }
+        if (!ConfigHandler.getInstance().getSettingsConfig().getBoolean("enderbutt.enabled")) return;
+
+        if (!player.getItemInHand().equals(HotbarItem.ENDER_BUTT.getItem())) return;
 
         player.setVelocity(player.getLocation().getDirection().normalize().multiply(2.5F));
         player.playSound(player.getLocation(), Sound.ENTITY_ENDER_PEARL_THROW, 1.0F, 1.0F);
